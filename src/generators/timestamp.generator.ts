42b8a8cdeaac391a9739ab14bed81256471f055c
CHIKIRIAY <db@hookprod.com>
2023-05-08 00:34:47 +0300

    fix: timestamp generator

--- src/generators/timestamp.generator.ts
+++ src/generators/timestamp.generator.ts
@@ -3,10 +3,10 @@
  * @created: 5/7/23
  * @Time: 9:01 PM
  */
-import { generateNumber } from "./number.generator";
+import { generateNumber, RoundingStrategy } from "./number.generator";
 
 export const generateTimestamp = ({ minDate, maxDate }: TimestampGeneratorType): Date => {
-	return new Date(generateNumber({ min: +minDate, max: +maxDate, round: true }));
+	return new Date(generateNumber(+minDate, +maxDate, RoundingStrategy.ROUND));
 };
 
 export type TimestampGeneratorType = {
