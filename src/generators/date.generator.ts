16d804885639c12ef5bf0127c5f7adcfd3409248
CHIKIRIAY <db@hookprod.com>
2023-05-08 12:45:41 +0300

    feat: date generator

---
+++ src/generators/date.generator.ts
@@ -0,0 +1,18 @@
+/**
+ * @author: CHIKIRIAY
+ * @created: 5/7/23
+ * @Time: 9:01 PM
+ */
+import { generateNumber, RoundingStrategy } from "./number.generator";
+import { generateMany } from "../utils/generate-many.util";
+import { ValueDate } from "../types/objectValueType";
+
+export const generateDate = ({ min, max }: Omit<ValueDate, "type">): Date => {
+	return new Date(generateNumber({
+		min: +min, max: +max, rounding: RoundingStrategy.ROUND,
+	}));
+};
+
+export const generateManyDates = (count: number, params: Omit<ValueDate, "type">): Date[] => {
+	return generateMany<Date>(count, () => generateDate(params));
+};
