9ac817ece74fdc661a3f758fca49397909252f9e
CHIKIRIAY <db@hookprod.com>
2023-05-09 03:49:19 +0300

    fix: split get client and insert many

--- src/mongo.ts
+++ src/mongo.ts
@@ -3,26 +3,26 @@
  * @created: 5/7/23
  * @Time: 8:44 PM
  */
-import { BulkWriteResult, Collection, MongoClient } from "mongodb";
-import { Envs } from "@Envs";
+import { BulkWriteOptions, BulkWriteResult, Collection, MongoClient } from "mongodb";
+import { Envs } from "./config/envs";
+
+export const getClient = (): Promise<MongoClient> => {
+	return MongoClient.connect(Envs.MONGODB_CONNECTION_URL);
+};
 
 export const insertMany = async <T>(
+	client: MongoClient,
 	collectionName: string,
 	object: T[],
+	options?: BulkWriteOptions,
 ): Promise<BulkWriteResult> => {
-	const client = await MongoClient.connect(Envs.MONGODB_CONNECTION_URL);
 	const db = client.db(Envs.MONGODB_DATABASE_NAME);
 	const collection: Collection<T> = db.collection(collectionName);
 
 	const bulk = collection.initializeUnorderedBulkOp();
-
 	object.forEach((person) => {
 		bulk.insert(person);
 	});
 
-	const result = await bulk.execute();
-
-	await client.close();
-
-	return result;
+	return await bulk.execute(options);
 };
